ecab48b2d6e05c464d60d066644c3c6b88126ef3
Arbri çoçka <35626219+cocka11@users.noreply.github.com>
2024-10-05 22:46:30 +0200

    Update and rename post-fs-data.sh to customize.sh

---
+++ customize.sh
@@ -0,0 +1,2 @@
+#!/system/bin/sh
+set_perm /system/bin/boot_scripts.sh 0 0 0755
--- post-fs-data.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-chmod 755 "/system/xbin/boot_script.sh"
-chmod 755 "$MODPATH/system/xbin/boot_script.sh"
-
