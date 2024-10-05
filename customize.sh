0fdd26bf26788e8cbd8980865c05f9ad8ef1e916
Arbri çoçka <35626219+cocka11@users.noreply.github.com>
2024-10-05 18:57:15 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,9 +1,4 @@
 #!/system/bin/sh
 
-mount -o rw,remount /
-
 set_perm /system/etc/init/boot_script.rc 0 0 0644
-if [ ! -d /system/xbin ]; them
-   mkdir /system/xbin
-   set_perm /system/etc/init/boot_script.sh 0 0 0755
-fi
+set_perm /system/etc/init/boot_script.sh 0 0 0755
