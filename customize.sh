f6f5fb0d4693e2513f08c0986d2134641086878f
Arbri çoçka <35626219+cocka11@users.noreply.github.com>
2024-10-06 11:34:41 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
 
 MODDIR=${0%/*}
-chown 0:0 $MODDIR/common/boot_script.sh;
-chmod 775 $MODDIR/common/boot_script.sh;
+chown 0:0 $MODDIR/common/post-fs-data.sh.sh;
+chmod 775 $MODDIR/common/post-fs-data.sh.sh;
