e806daaf65fa2b055bac2586242b36723c5b7d4e
Arbri çoçka <35626219+cocka11@users.noreply.github.com>
2024-10-08 19:40:09 +0200

    Fix permissons for init.d

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,9 +2,13 @@
 
 MODDIR=${0%/*}
 SCRIPT="$MODDIR/system/bin/boot_script.sh"
-SCRIPT_init="$MODDIR/system/etc/init.d/*
-chown 0:0 "$SCRIPT_init"
-chmod 775 "$SCRIPT_init"
+SCRIPT_init="$MODDIR/system/etc/init.d"
+
+for PERM in "$SCRIPT_init"/*; do
+    chown 0:0 "$PERM"
+    chmod 755 "$PERM"
+done
+
 # Check if the script exists
 if [ -f "$SCRIPT" ]; then
     # Change ownership and check for errors
