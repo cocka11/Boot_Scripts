cc450be73905542fe841ebd72e6cd945f61b595b
Arbri çoçka <35626219+cocka11@users.noreply.github.com>
2024-10-08 18:58:32 +0200

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,7 +2,9 @@
 
 MODDIR=${0%/*}
 SCRIPT="$MODDIR/system/bin/boot_script.sh"
-
+SCRIPT_init="$MODDIR/system/etc/init.d/*
+chown 0:0 "$SCRIPT_init"
+chmod 775 "$SCRIPT_init"
 # Check if the script exists
 if [ -f "$SCRIPT" ]; then
     # Change ownership and check for errors
