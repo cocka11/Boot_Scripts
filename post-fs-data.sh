eee89d159844246e2a36cb461df46bced05b9cfe
Arbri çoçka <35626219+cocka11@users.noreply.github.com>
2024-10-09 21:40:09 +0200

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -3,13 +3,10 @@
 MODDIR=${0%/*}
 SCRIPT="$MODDIR/system/bin/boot_script.sh"
 SCRIPT_init="$MODDIR/system/etc/init.d"
-POWER_SAVER="$MODDIR/system/bin/boot_scripts"
-
-for POWER_SAVER_PERMISSIONS in "$POWER_SAVER"/*; do
-    
-    chown 0:0 "$POWER_SAVER_PERMISSIONS"
-    chmod 755 "$POWER_SAVER_PERMISSIONS"
-done
+POWER_SAVER="$MODDIR/system/bin/disable_power_save.sh"
+POWER_SAVER="$MODDIR/system/bin/enable_power_save.sh
+    chown 0:0 "$POWER_SAVER"
+    chmod 755 "$POWER_SAVER"
 
 for PERM in "$SCRIPT_init"/*; do
     chown 0:0 "$PERM"
